565309cf69324ccdf0418d98494402ce62703ee5
Jyotirmoy <jyotirmoykarmakar02@gmail.com>
2023-09-12 13:52:51 +0530

    server sidebar

---
+++ app/(main)/(routes)/servers/[serverId]/layout.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import { currentProfile } from "@/lib/currentProfile";
+import ServerSidebar from "@/components/server/ServerSidebar";
+
+const ServerIdLayout = async ({ children, params }) => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirectToSignIn();
+  }
+
+  const server = await db.server.findUnique({
+    where: {
+      id: params.serverId,
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (!server) {
+    return redirect("/");
+  }
+
+  return (
+    <div className='h-full'>
+      <div className='hidden md:flex h-full w-60 z-20 flex-col fixed inset-y-0'>
+        <ServerSidebar serverId={params.serverId} />
+      </div>
+
+      <main className='h-full md:pl-60'>{children}</main>
+    </div>
+  );
+};
+
+export default ServerIdLayout;
