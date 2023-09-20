b74a4b94f5b32853d4cae0b8acf8eb881822bb0f
Jyotirmoy <jyotirmoykarmakar02@gmail.com>
2023-09-20 14:37:32 +0530

    delete and leave server

--- app/api/servers/[serverId]/route.js
+++ app/api/servers/[serverId]/route.js
@@ -23,3 +23,23 @@ export async function PATCH(req, { params }) {
     return new NextResponse("Internal error", { status: 500 });
   }
 }
+
+export async function DELETE(req, { params }) {
+  try {
+    const profile = await currentProfile();
+
+    if (!profile) return new NextResponse("Unauthorized", { status: 401 });
+
+    const server = await db.server.delete({
+      where: {
+        id: params.serverId,
+        profileId: profile.id,
+      },
+    });
+
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log("delete -->", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
