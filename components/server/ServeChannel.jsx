1470bbfc4a60c9b7c40fff9d28d27c7eb0188ead
Jyotirmoy <jyotirmoykarmakar02@gmail.com>
2023-09-23 14:14:25 +0530

    channel Id page

--- components/server/ServeChannel.jsx
+++ components/server/ServeChannel.jsx
@@ -21,9 +21,19 @@ const ServeChannel = ({ channel, server, role }) => {
 
   const Icon = iconMap[channel.type];
 
+  const onClickHandler = () => {
+    router.push(`/servers/${params?.serverId}/channels/${channel?.id}`);
+    // console.log(channel?.id);
+  };
+
+  const onAction = (e, action) => {
+    e.stopPropagation();
+    onOpen(action, { channel, server });
+  };
+
   return (
     <button
-      onClick={() => {}}
+      onClick={onClickHandler}
       className={cn(
         "group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1",
         params?.channelId === channel.id && "bg-zinc-700/20 dark:bg-zinc-700"
@@ -45,14 +55,14 @@ const ServeChannel = ({ channel, server, role }) => {
           <ActionTooltip label='Edit'>
             <Edit
               className='w-4 h-4 hidden group-hover:block text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition'
-              onClick={() => onOpen("editChannel", { server, channel })}
+              onClick={(e) => onAction(e, "editChannel")}
             />
           </ActionTooltip>
 
           <ActionTooltip label='Delete'>
             <Trash
               className='w-4 h-4 hidden group-hover:block text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition'
-              onClick={() => onOpen("deleteChannel", { server, channel })}
+              onClick={(e) => onAction(e, "deleteChannel")}
             />
           </ActionTooltip>
         </div>
