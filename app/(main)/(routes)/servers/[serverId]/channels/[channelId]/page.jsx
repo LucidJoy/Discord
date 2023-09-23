570f352511c079702a7adec24bfd228feee7153c
Jyotirmoy <jyotirmoykarmakar02@gmail.com>
2023-09-23 14:42:49 +0530

    chat header

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.jsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import { currentProfile } from "@/lib/currentProfile";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-const ChannelId = () => {
-  return <div>ChannelId</div>;
+import db from "@/lib/db";
+import ChatHeader from "@/components/chat/ChatHeader";
+
+const ChannelId = async ({ params }) => {
+  const profile = await currentProfile();
+
+  if (!profile) return redirectToSignIn();
+
+  const channel = await db.channel.findUnique({
+    where: {
+      id: params.channelId,
+    },
+  });
+
+  const member = await db.member.findFirst({
+    where: {
+      id: params.serverId,
+      profileId: profile.id,
+    },
+  });
+
+  if (!channel || !channel) redirect("/");
+
+  return (
+    <div className='bg-white dark:bg-[#313338] flex flex-col h-full'>
+      <ChatHeader
+        name={channel.name}
+        serverId={channel.serverId}
+        type='channel'
+      />
+    </div>
+  );
 };
 
 export default ChannelId;
